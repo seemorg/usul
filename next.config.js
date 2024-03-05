f3015d227d33c3f8f541f9ee3317e8c5e154d263
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-03-05 05:24:26 +0200

    add redirects

--- next.config.js
+++ next.config.js
@@ -9,6 +9,35 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  redirects: async () => {
+    return [
+      {
+        // Redirects from /regions/:region to /region/:region
+        source: "/regions/:region",
+        destination: "/region/:region",
+        permanent: true,
+      },
+      {
+        // Redirects from /authors/:authorSlug to /author/:authorSlug
+        source: "/authors/:authorSlug",
+        destination: "/author/:authorSlug",
+        permanent: true,
+      },
+      {
+        // Redirects from /centuries/:century to /century/:century
+        source: "/centuries/:century",
+        destination: "/century/:century",
+        permanent: true,
+      },
+      {
+        // Redirects from /genres/:genreSlug to /genre/:genreSlug
+        source: "/genres/:genreSlug",
+        destination: "/genre/:genreSlug",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default withNextIntl(config);
