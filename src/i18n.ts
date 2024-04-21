36c61a511d9d2b3f08eaa2f2c38a1d67f6e0eb2c
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-04-22 01:48:33 +0200

    add other locales

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,10 +2,13 @@ import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
 import config, { locales, type AppLocale } from "i18n.config";
 import type { IntlConfig } from "next-intl";
+import { supportedBcp47LocaleToPathLocale } from "./lib/locale/utils";
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as AppLocale)) notFound();
+
+  const pathLocale = supportedBcp47LocaleToPathLocale(locale as AppLocale);
 
   const messages = (
     await Promise.all(
@@ -13,7 +16,7 @@ export default getRequestConfig(async ({ locale }) => {
         const messages = {
           // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
           [namespace]: (
-            (await import(`../locales/${locale}/${namespace}.json`)) as {
+            (await import(`../locales/${pathLocale}/${namespace}.json`)) as {
               default: Record<string, string>;
             }
           ).default,
