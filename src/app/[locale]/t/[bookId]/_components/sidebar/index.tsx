cf796659532d9a229ac25e533897e33001218b95
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-06-28 11:19:59 +0300

    add gradient for ai tab

--- src/app/[locale]/t/[bookId]/_components/sidebar/index.tsx
+++ src/app/[locale]/t/[bookId]/_components/sidebar/index.tsx
@@ -33,6 +33,135 @@ import { tabs } from "./tabs";
 //   );
 // };
 
+function AnimatedGradientText({
+  children,
+  className,
+  isActive,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  isActive?: boolean;
+}) {
+  return (
+    <div
+      className={cn(
+        "group relative cursor-pointer [--bg-size:300%]",
+        className,
+      )}
+    >
+      <div
+        className={cn(
+          isActive
+            ? "animate-gradient pointer-events-none absolute inset-0 -z-[1] block h-full w-full bg-gradient-to-r from-[#ffaa40]/50 via-[#9c40ff]/50 to-[#ffaa40]/50 bg-[length:var(--bg-size)_100%] p-[1px] [border-radius:inherit]"
+            : "animate-gradient pointer-events-none absolute inset-0 block h-full w-full bg-gradient-to-r from-[#ffaa40]/50 via-[#9c40ff]/50 to-[#ffaa40]/50 bg-[length:var(--bg-size)_100%] p-[1px] [border-radius:inherit] ![mask-composite:subtract] [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)]",
+        )}
+      />
+
+      {children}
+    </div>
+  );
+}
+
+const SparklesIcon = ({
+  className,
+  isGradient,
+}: {
+  className?: string;
+  isGradient?: boolean;
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      className={cn(className)}
+      fill={!isGradient ? "currentColor" : undefined}
+    >
+      {isGradient && (
+        <defs>
+          <linearGradient id="grad1" x1="0%" y1="100%" x2="100%" y2="0%">
+            <stop
+              offset="0%"
+              style={{
+                stopColor: "#ffaa40",
+              }}
+            />
+            <stop
+              offset="50%"
+              style={{
+                stopColor: "#9c40ff",
+              }}
+            />
+            <stop
+              offset="100%"
+              style={{
+                stopColor: "#ffaa40",
+              }}
+            />
+          </linearGradient>
+        </defs>
+      )}
+
+      <path
+        d="M9 4.5a.75.75 0 0 1 .721.544l.813 2.846a3.75 3.75 0 0 0 2.576 2.576l2.846.813a.75.75 0 0 1 0 1.442l-2.846.813a3.75 3.75 0 0 0-2.576 2.576l-.813 2.846a.75.75 0 0 1-1.442 0l-.813-2.846a3.75 3.75 0 0 0-2.576-2.576l-2.846-.813a.75.75 0 0 1 0-1.442l2.846-.813A3.75 3.75 0 0 0 7.466 7.89l.813-2.846A.75.75 0 0 1 9 4.5ZM18 1.5a.75.75 0 0 1 .728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 0 1 0 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 0 1-1.456 0l-.258-1.036a2.625 2.625 0 0 0-1.91-1.91l-1.036-.258a.75.75 0 0 1 0-1.456l1.036-.258a2.625 2.625 0 0 0 1.91-1.91l.258-1.036A.75.75 0 0 1 18 1.5ZM16.5 15a.75.75 0 0 1 .712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 0 1 0 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 0 1-1.422 0l-.395-1.183a1.5 1.5 0 0 0-.948-.948l-1.183-.395a.75.75 0 0 1 0-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0 1 16.5 15Z"
+        fill={isGradient ? "url(#grad1)" : undefined}
+        fillRule="evenodd"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+const TabButton = ({
+  tab,
+  bookId,
+}: {
+  tab: (typeof tabs)[number];
+  bookId: string;
+}) => {
+  const t = useTranslations();
+  const isActive = usePathname().endsWith("/ai");
+  const isHighlighted = tab.id === "ai";
+  const IconToUse = isHighlighted ? SparklesIcon : tab.icon;
+
+  const icon = (
+    <IconToUse
+      isGradient={isHighlighted && !isActive}
+      className={cn(
+        "z-[10] size-5",
+        isHighlighted && isActive && "text-white",
+        // isHighlighted &&
+        //   "animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-padding text-transparent",
+      )}
+    />
+  );
+
+  const link = (
+    <Link href={tab.href(bookId)} className="z-[1]">
+      <TooltipTrigger>{isHighlighted ? icon : icon}</TooltipTrigger>
+    </Link>
+  );
+
+  const trigger = (
+    <TabsTrigger value={tab.id} className={cn("w-full py-1.5")} asChild>
+      {isHighlighted ? (
+        <AnimatedGradientText isActive={isActive}>{link}</AnimatedGradientText>
+      ) : (
+        link
+      )}
+    </TabsTrigger>
+  );
+
+  return (
+    <Tooltip>
+      {trigger}
+
+      <TooltipContent side="bottom" sideOffset={10}>
+        {t(tab.label as any)}
+      </TooltipContent>
+    </Tooltip>
+  );
+};
+
 export default function ReaderSidebar({
   children,
   bookId,
@@ -41,8 +170,6 @@ export default function ReaderSidebar({
   bookId: string;
 }) {
   const path = usePathname();
-  const t = useTranslations();
-
   const activeTab = tabs.find((tab) => path === tab.href(bookId))?.id;
 
   return (
@@ -54,27 +181,7 @@ export default function ReaderSidebar({
         <SidebarContainer className="hidden sm:block">
           <TabsList className="h-10 w-full rounded-b-none font-sans">
             {tabs.map((tab) => (
-              <Tooltip key={tab.id}>
-                <TabsTrigger
-                  value={tab.id}
-                  className={cn(
-                    "w-full py-1.5",
-                    tab.id === "ai" &&
-                      "border-2 border-primary data-[state=active]:bg-primary data-[state=active]:text-white",
-                  )}
-                  asChild
-                >
-                  <Link href={tab.href(bookId)}>
-                    <TooltipTrigger>
-                      <tab.icon className="h-5 w-5" />
-                    </TooltipTrigger>
-                  </Link>
-                </TabsTrigger>
-
-                <TooltipContent side="bottom" sideOffset={10}>
-                  {t(tab.label as any)}
-                </TooltipContent>
-              </Tooltip>
+              <TabButton key={tab.id} tab={tab} bookId={bookId} />
             ))}
           </TabsList>
         </SidebarContainer>
