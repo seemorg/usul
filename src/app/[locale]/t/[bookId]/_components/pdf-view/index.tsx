6754b45a3ffbef968f7ab3cd909619f188762712
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-10-08 21:34:52 +0300

    fix pdf buttons

--- src/app/[locale]/t/[bookId]/_components/pdf-view/index.tsx
+++ src/app/[locale]/t/[bookId]/_components/pdf-view/index.tsx
@@ -12,6 +12,7 @@ import { useEffect, useRef } from "react";
 import { type PdfChapter, usePdfChapterStore } from "./store";
 import { useTheme } from "next-themes";
 import { env } from "@/env";
+import { makePdfViewerButtons } from "./buttons";
 
 const isInitializedByUrl = new Map<string, boolean>();
 
@@ -39,12 +40,14 @@ export default function PdfView({
       const instance: WebViewerInstance = await WebViewer(
         {
           path: "/pdf-express", // point to where the files you copied are served from
-          initialDoc: pdfSource.finalUrl!, // path to your document
+          // initialDoc: pdfSource.finalUrl!, // path to your document
+          initialDoc: "/demo.pdf",
           enableAnnotations: false,
           disabledElements: [
+            // "selectToolButton",
             "leftPanel",
             "leftPanelButton",
-            // "selectToolButton",
+            "menuButton",
             "themeChangeButton",
             "languageButton",
           ],
@@ -55,66 +58,11 @@ export default function PdfView({
         viewerRef.current!,
       );
 
-      const downloadButton = {
-        type: "actionButton",
-        img: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.cls-1{fill:#abb0c4;}</style></defs><title>icon - header - download</title><path class="cls-1" d="M11.55,17,5.09,9.66a.6.6,0,0,1,.45-1H8.67V2.6a.6.6,0,0,1,.6-.6h5.46a.6.6,0,0,1,.6.6V8.67h3.13a.6.6,0,0,1,.45,1L12.45,17A.6.6,0,0,1,11.55,17ZM3.11,20.18V21.6a.4.4,0,0,0,.4.4h17a.4.4,0,0,0,.4-.4V20.18a.4.4,0,0,0-.4-.4h-17A.4.4,0,0,0,3.11,20.18Z"></path></svg>`,
-        onClick: () => {
-          instance.UI.downloadPdf();
-        },
-        dataElement: "downloadButton",
-      };
-
-      //       const fullscreenButton: Web = {
-      //   type: 'statefulButton',
-      //   initialState: 'minimized',
-      //   states: {
-      //     minimized: {
-      //       img: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.cls-1{fill:#abb0c4;}</style></defs><title>icon - header - full screen</title><path class="cls-1" d="M4.22,4.22H9.78V2H2V9.78H4.22ZM9.78,19.78H4.22V14.22H2V22H9.78ZM22,14.22H19.78v5.56H14.22V22H22ZM19.78,9.78H22V2H14.22V4.22h5.56Z"></path></svg>`,
-      //       onClick: (update, activeState) => {
-      //         activeState.number += 1;
-      //         update();
-      //       }
-      //     },
-      //     full: {
-      //       img: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.cls-1{fill:#abb0c4;}</style></defs><title>icon - header - full screen - exit</title><path class="cls-1" d="M9.5,2H7V7H2V9.5H9.5ZM7,22H9.5V14.5H2V17H7Zm15-7.5H14.5V22H17V17h5ZM22,7H17V2H14.5V9.5H22Z"></path></svg>`,
-      //       onClick: (update, activeState) => {
-      //         activeState.number += 1;
-      //         update();
-      //       }
-      //     }
-      //   },
-      //   mount: update => {
-      //     const fitModeToState = fitMode => {
-      //       // the returned state should be the opposite of the new current state
-      //       // as the opposite state is what we want to switch to when the button
-      //       // is clicked next
-      //       if (fitMode === instance.UI.FitMode.FitPage) {
-      //         return 'FitWidth';
-      //       } else if (fitMode === instance.UI.FitMode.FitWidth) {
-      //         return 'FitPage';
-      //       }
-      //     };
-
-      //     instance.Core.documentViewer.addEventListener('fitModeUpdated.fitbutton', fitMode => {
-      //       update(fitModeToState(fitMode));
-      //     });
-      //   },
-      //   unmount: () => {
-      //     instance.Core.documentViewer.removeEventListener('fitModeUpdated.fitbutton');
-      //   },
-      //   dataElement: 'countButton'
-      // };
-
-      // full screen icon
-      // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.cls-1{fill:#abb0c4;}</style></defs><title>icon - header - full screen</title><path class="cls-1" d="M4.22,4.22H9.78V2H2V9.78H4.22ZM9.78,19.78H4.22V14.22H2V22H9.78ZM22,14.22H19.78v5.56H14.22V22H22ZM19.78,9.78H22V2H14.22V4.22h5.56Z"></path></svg>
-
-      // exit full screen
-      // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.cls-1{fill:#abb0c4;}</style></defs><title>icon - header - full screen - exit</title><path class="cls-1" d="M9.5,2H7V7H2V9.5H9.5ZM7,22H9.5V14.5H2V17H7Zm15-7.5H14.5V22H17V17h5ZM22,7H17V2H14.5V9.5H22Z"></path></svg>
-
       instanceRef.current = instance;
 
       instance.UI.setHeaderItems((headers) => {
-        headers.push(downloadButton);
+        const buttons = makePdfViewerButtons(instance);
+        buttons.forEach((b) => headers.push(b));
       });
 
       instance.UI.setTheme(
@@ -122,9 +70,6 @@ export default function PdfView({
           ? instance.UI.Theme.DARK
           : instance.UI.Theme.LIGHT,
       );
-
-      // enable text selection
-      // instance.Core.documentViewer.getSelectedText(true);
 
       instance.Core.documentViewer.addEventListener(
         "documentLoaded",
