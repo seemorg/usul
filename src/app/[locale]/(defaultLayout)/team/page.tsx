b302d570cda1be4b9c4c1a036d4bef60b84a3277
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-10-10 12:48:53 +0300

    update team details

--- src/app/[locale]/(defaultLayout)/team/page.tsx
+++ src/app/[locale]/(defaultLayout)/team/page.tsx
@@ -4,6 +4,79 @@ import { HeartHandshakeIcon, MailIcon } from "lucide-react";
 import { getTranslations } from "next-intl/server";
 import MemberCard from "./member-card";
 
+const members = [
+  {
+    name: "Abdellatif Abdelfattah",
+    role: "CEO",
+    description:
+      "Abdellatif co-founded Tarteel AI, lead engineering at Quran.com, and worked at Twitter.",
+    image: "",
+  },
+  {
+    name: "Ahmed Riad",
+    role: "Founding Engineer",
+    description:
+      "Ahmed co-founded Remail AI, Betterbook, and was an engineer at Quran.com.",
+    image: "",
+  },
+  {
+    name: "Ryad Ramo",
+    role: "Researcher",
+    description:
+      "Ryad specializes in Maliki fiqh and Andulisian hisory. He co-managed Markaz Imam Malik.",
+    image: "",
+  },
+  {
+    name: "Ismail Safadi",
+    role: "Researcher",
+    description:
+      "Ismail specializes in Hanafi fiqh. He studied at the University of Jordan and in Turkey.",
+    image: "",
+  },
+  {
+    name: "Ahmed Noor",
+    role: "Researcher",
+    description:
+      "Ahmed is an Islamic theology student at İZÜ. He’s fluent in Arabic, English, and in Urdu.",
+    image: "",
+  },
+  {
+    name: "Ahmed Khan",
+    role: "Researcher",
+    description:
+      "Ahmed is a student at Zaytuna College. He’s the creator of The Creative Minority podcast.",
+    image: "",
+  },
+  {
+    name: "Ahmet Aktan",
+    role: "Digitization Lead",
+    description:
+      "Ahmed holds a PhD. from Al-Azhar university. He leads the digitization team and efforts.",
+    image: "",
+  },
+];
+
+const boardMembers = [
+  {
+    name: "Adnan Zulfiqar",
+    description:
+      "Adnan specializes in Islamic and criminal law. He drafted criminal codes for Maldives and Somalia.",
+    image: "",
+  },
+  {
+    name: "Intisar Rabb",
+    description:
+      "Intisar is a Law professor at Harvard. She leads SHARIAsource and the Program in Islamic Law.",
+    image: "",
+  },
+  {
+    name: "Zeki Mokhtarzada",
+    description:
+      "Zeki is an enterprenuer with with deep technical expertise. He co-founded Freewebs and Truebill.",
+    image: "",
+  },
+];
+
 export default async function TeamPage() {
   const t = await getTranslations("about");
 
@@ -30,74 +103,36 @@ export default async function TeamPage() {
       </div>
 
       <div className="mt-20 grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-        <MemberCard
-          name="Abdellatif Abdelfattah"
-          role="CEO"
-          description="Abdellatif is a serial enterprenuer. Before Usul, he lead engineering at Quran.com."
-          image=""
-        />
-        <MemberCard
-          name="Ahmed Riad"
-          role="Founding Engineer"
-          description="Abdellatif is a serial enterprenuer. Before Usul, he lead engineering at Quran.com."
-          image=""
-        />
-        <MemberCard
-          name="Gene Ryaz"
-          role="Researcher"
-          description="Abdellatif is a serial enterprenuer. Before Usul, he lead engineering at Quran.com."
-          image=""
-        />
-        <MemberCard
-          name="Ismail Safadi"
-          role="Researcher"
-          description="Abdellatif is a serial enterprenuer. Before Usul, he lead engineering at Quran.com."
-          image=""
-        />
-        <MemberCard
-          name="Ahmed Noor"
-          role="Researcher"
-          description="Abdellatif is a serial enterprenuer. Before Usul, he lead engineering at Quran.com."
-          image=""
-        />
-        <MemberCard
-          name="Ahmed Khan"
-          role="Researcher"
-          description="Abdellatif is a serial enterprenuer. Before Usul, he lead engineering at Quran.com."
-          image=""
-        />
-        <MemberCard
-          name="Ahmet Aktan"
-          role="Digitization Lead"
-          description="Abdellatif is a serial enterprenuer. Before Usul, he lead engineering at Quran.com."
-          image=""
-        />
+        {members.map((member) => (
+          <MemberCard
+            key={member.name}
+            name={member.name}
+            role={member.role}
+            description={member.description}
+            image={member.image}
+          />
+        ))}
       </div>
 
-      <div className="mb-20 mt-24">
+      <div className="mb-20 mt-44">
         <h1 className="text-5xl font-bold">Meet the board</h1>
         <p className="mt-5">
-          The Seemorg Foundation board is made up of research and technology
-          experts. They built experience over decades and are now bringing it to
-          Usul.
+          The Seemorg Foundation’s board is made up of distinguished research
+          and technology experts, each with decades of experience in their
+          respective fields. Together, they oversee the organization and are
+          ensure that it advances towards its mission of advancing Islamic
+          scholarship.
         </p>
 
         <div className="mt-14 grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-          <MemberCard
-            name="Adnan Zulfiqar"
-            description="Abdellatif is a serial enterprenuer. Before Usul, he lead engineering at Quran.com."
-            image=""
-          />
-          <MemberCard
-            name="Intisar Rabb"
-            description="Abdellatif is a serial enterprenuer. Before Usul, he lead engineering at Quran.com."
-            image=""
-          />
-          <MemberCard
-            name="Zeki Mokhtarzada"
-            description="Abdellatif is a serial enterprenuer. Before Usul, he lead engineering at Quran.com."
-            image=""
-          />
+          {boardMembers.map((member) => (
+            <MemberCard
+              key={member.name}
+              name={member.name}
+              description={member.description}
+              image={member.image}
+            />
+          ))}
         </div>
       </div>
     </Container>
