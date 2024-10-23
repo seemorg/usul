3700fb25d1efd53b9bc6cbadd3079b473daa9c7c
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-10-23 14:32:25 +0300

    update prisma schema & add advanced genres pdf

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,38 @@
 declare global {
   namespace PrismaJson {
-    type BookVersion = {
-      source: "openiti" | "turath" | "external";
+    interface BookVersion {
+      source: "openiti" | "turath" | "external" | "pdf";
       value: string;
-    };
+      publicationDetails?: {
+        investigator?: string;
+        publisher?: string;
+        editionNumber?: string;
+        publicationYear?: number; // hijri
+      };
+    }
 
-    type BookFlags = {
+    interface BookFlags {
       aiSupported?: boolean;
       aiVersion?: string;
-    };
+    }
+
+    interface AuthorExtraProperties {
+      _airtableReference?: string;
+    }
+
+    interface BookExtraProperties {
+      splitsData?: { start: number; end: number }[];
+      _airtableReference?: string;
+    }
+
+    interface GenreExtraProperties {
+      _airtableReference?: string;
+    }
+
+    interface AdvancedGenreExtraProperties {
+      _airtableReference?: string;
+      simpleGenreId?: string; // id in Genres table
+    }
   }
 }
 
