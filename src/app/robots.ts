78bce8a2b86dda104ef7a57dcbe82cbb7f489e99
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-11-03 07:42:50 +0200

    disable indexing for non-prod environments & add container padding for empty page navigator

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,22 @@
+import { env } from "@/env";
 import { config } from "@/lib/seo";
 import type { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Robots {
+export default function robots(): MetadataRoute.Robots {
+  const isProd = env.VERCEL_ENV === "production";
+
+  if (!isProd) {
+    return {
+      rules: [
+        {
+          userAgent: "*",
+          disallow: "/",
+        },
+      ],
+      host: config.url,
+    };
+  }
+
   return {
     rules: [
       {
