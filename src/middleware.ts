e51cef467e2651e8b26bf8577b4e70908c63c7a0
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2025-09-06 23:41:28 +0300

    add _vercel to middleware ignore

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,10 +10,10 @@ export const config: MiddlewareConfig = {
     /*
      * Match all paths except for:
      * 1. /api/ routes
-     * 2. /_next/ (Next.js internals)
+     * 2. /_next/ and /_vercel/ (Next.js internals)
      * 3. /sitemap/ (sitemap files)
      * 4. Metadata files: favicon.ico, sitemap.xml, robots.txt, manifest.webmanifest
      */
-    "/((?!api/|_next/|favicon.ico|sitemap/|sitemap.xml|robots.txt|manifest.webmanifest).*)",
+    "/((?!api/|_next/|_vercel/|favicon.ico|sitemap/|sitemap.xml|robots.txt|manifest.webmanifest).*)",
   ],
 };
