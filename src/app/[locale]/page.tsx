428d1536d7a310f6438c5f72889e8cfd3d299084
Anas Ahmed <anas.ahassan@yahoo.com>
2025-12-09 22:42:23 +0200

    Publish Special and Individual collections

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -220,7 +220,6 @@ export default async function HomePage({
           />
         </div>
 
-        {/*
         <div>
           <HomepageSection
             isBooks={false}
@@ -262,7 +261,6 @@ export default async function HomePage({
             ))}
           />
         </div>
-        */}
 
         {/* 
         <div>
