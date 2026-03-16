633dd58db8624cfda56e0c6e9cec8427be27e8af
Anas Ahmed <anas.ahassan@yahoo.com>
2026-03-16 14:09:02 +0200

    empire and region hierarchical view

--- src/lib/api/empires.ts
+++ src/lib/api/empires.ts
@@ -1,4 +1,5 @@
 import type { ApiEmpire } from "@/types/api/empire";
+import type { HierarchicalItem } from "@/components/hierarchical-list-view";
 import { cache } from "react";
 
 import type { PathLocale } from "../locale/utils";
@@ -30,6 +31,19 @@ export const findAllEmpiresWithBooksCount = cache(
         locale,
         ...params,
       },
+    });
+
+    return result ?? [];
+  },
+);
+
+export const getEmpireHierarchy = cache(
+  async (locale: PathLocale = "en") => {
+    const result = await apiFetch<
+      (HierarchicalItem & { numberOfAuthors: number; numberOfBooks: number })[]
+    >({
+      path: "/empire/hierarchy",
+      params: { locale },
     });
 
     return result ?? [];
